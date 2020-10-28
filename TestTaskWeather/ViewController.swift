d2924b9f58f435d0242a199c879550144fead1f3
Artem <artemedmshot@gmail.com>
2020-10-28 16:43:27 +0300

    Initial Commit

---
+++ TestTaskWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTaskWeather
+//
+//  Created by Artem Pozdnyakov on 10/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
